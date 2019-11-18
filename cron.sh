015dd9345bf0a54f091c41b7f6fe1671292e19b7
Benjamin Hudgens <benjamin@benjamindavid.com>
2019-11-18 14:09:10 -0600

    Support two drives

--- cron.sh
+++ cron.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 
 exit 0
-export TMPDIR="/benchmarking"
-mkdir -p "${TMPDIR}"
-WORKING_DIR=$(mktemp -d)
-pushd "${WORKING_DIR}" > /dev/null
-/usr/bin/benchmark.sh
-/usr/bin/process.sh
-popd
+for drive in "/mnt/osdrive" "/mnt/mounteddrive"; do
+  export TMPDIR="${drive}"
+  mkdir -p "${TMPDIR}"
+  WORKING_DIR=$(mktemp -d)
+  pushd "${WORKING_DIR}" > /dev/null
+  /usr/bin/benchmark.sh
+  /usr/bin/process.sh
+  popd
+done
